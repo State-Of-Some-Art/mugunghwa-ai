eed241ad75f817f52c065dba9b95c54c3f54beb1
k2sebeom <moses97@gmail.com>
2021-10-16 22:59:34 +0900

    feat: Update documents

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # mugunghwa-ai
+
+Mugunghwa AI is an artificial intelligence that operates a "Mugunghwa-game" featured in a famous Netflix series "Squid Game."
+
+## Prerequisite
+------
+```
+$ pip install -r requirements.txt
+```
+and install appropriate version of [Pytorch](https://pytorch.org/get-started/locally/).
--- detect.py
+++ detect.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import imutils
+import time
 
 
 def smooth_frame(src):
@@ -15,15 +17,25 @@ def calc_diff(src1, src2):
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture(0)
-
+    time.sleep(3)
     ret, bg_frame = cap.read()
     bg_frame = smooth_frame(bg_frame)
 
     while ret:
         ret, frame = cap.read()
-        frame = smooth_frame(frame)
-        diff = calc_diff(frame, bg_frame)
-        cv2.imshow("asd", diff)
+        frame2 = smooth_frame(frame)
+        diff = calc_diff(frame2, bg_frame)
+
+        cnts = cv2.findContours(diff.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+
+        for c in cnts:
+            if cv2.contourArea(c) < 8000:
+                continue
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        cv2.imshow("asd", frame)
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
