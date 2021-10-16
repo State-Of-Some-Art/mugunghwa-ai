62f6e832a2f20d125b0d859621a3e703637b6c79
k2sebeom <moses97@gmail.com>
2021-10-17 00:09:20 +0900

    feat: Add unittest skeleton

---
+++ tests/test_detection.py
@@ -0,0 +1,11 @@
+import unittest
+from unittest.main import main
+
+class TestDetection(unittest.TestCase):
+    def test_dection(self):
+        print("Hello World")
+        self.assertEqual(1, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
